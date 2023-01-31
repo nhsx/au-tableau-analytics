0fb3250f8d0f2f21e8733bf6d57cfe1eda3002d1
Oli <oliver.jones@nhsx.nhs.uk>
2023-01-31 17:11:31 +0000

    Date filter refresh for DSCR

--- views/DSCR/nhs_dscr_2_view.sql
+++ views/DSCR/nhs_dscr_2_view.sql
@@ -1,7 +1,7 @@
 CREATE OR ALTER VIEW nhs_dscr_2_view AS 
 SELECT
 
-DATEFROMPARTS(RIGHT(month_year, 4),LEFT(month_year, 1),1) as _date
+DATEFROMPARTS(RIGHT(month_year, 4),LEFT(month_year, CHARINDEX('-',month_year)-1),1) as _date
 ,*
 
 
