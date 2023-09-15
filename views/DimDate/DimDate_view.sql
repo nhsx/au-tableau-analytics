274bd26ac2f0e499295abe5b47105bf4d354ca51
Oli <oliver.jones@nhsx.nhs.uk>
2023-09-15 10:27:47 +0100

    NDC updates

--- views/DimDate/DimDate_view.sql
+++ views/DimDate/DimDate_view.sql
@@ -1,5 +1,11 @@
 CREATE OR ALTER VIEW DimDate_View as 
 SELECT
     *
+    , case
+    when month >=4 then DATEADD(YEAR,1,[Date])
+    else [Date]
+    end
+    as FY_date
+
 FROM
     DimDate
---
+++ views/NDC/national_digital_channels_forecast_view.sql
@@ -0,0 +1,5 @@
+CREATE OR ALTER VIEW national_digital_channels_forecast_view AS
+SELECT
+    *
+FROM
+ndc_forecasts_all
